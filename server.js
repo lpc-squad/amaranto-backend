822646f8e85003802b3c97a2baea1bd8b758f163
FMGordillo <facundomgordillo@gmail.com>
2020-04-02 01:06:55 -0300

    Current status
    Co-authored-by: Karina Ledesma <ledesmakv@gmail.com>
    Co-authored-by: SantiagoIvan <santiago.feijoo96@gmail.com>

--- server.js
+++ server.js
@@ -1,36 +1,55 @@
 require("dotenv").config();
-const fs = require("fs");
-const https = require("https");
+const cors = require("cors");
 const express = require("express");
-const { auth, requiresAuth } = require("express-openid-connect");
 
-const key = fs.readFileSync("./localhost-key.pem");
-const cert = fs.readFileSync("./localhost.pem");
+const jwks = require("jwks-rsa");
+const jwt = require("express-jwt");
 
 const app = express();
 
-const config = {
-  required: false,
-  auth0Logout: true,
-  baseURL: "https://localhost:3000",
-  issuerBaseURL: process.env.issuerBaseURL,
-  clientID: process.env.clientId,
-  appSession: {
-    secret: process.env.appSessionSecret
-  }
-};
+const port = process.env.PORT || 8080;
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+/**
+ * SETEO
+ */
 
-// req.isAuthenticated is provided from the auth router
-app.get("/", (req, res) => {
-  res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    audience: process.env.audience,
+    issuer: process.env.issuer,
+    algorithms: ["RS256"]
+  })
 });
 
-app.get("/profile", requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.openid.user));
+app.use(
+  cors({
+    origin: "http://localhost:3000"
+  })
+);
+//app.use(jwtCheck);
+
+/**
+ * RUTAS (https://expressjs.com/en/starter/basic-routing.html)
+ * 1) Que datos queremos acceder
+ * { records, patients }
+ * 2) Como los queremos acceder
+ * { allRecords, recordsById, allPatients, patientsById, (patientsByDoctorId) }
+ * 3) Hacerlos >:v
+ */
+
+// app.METHOD(PATH, HANDLER) // DOCUMENTACION!
+app.get("/authorized", function onDone(req, res) {
+  res.send("Secured Resource");
 });
-https.createServer({ key, cert }, app).listen("3000", () => {
-  console.log("Listening on https://localhost:3000");
+
+// Patients
+
+// Records
+app.get("/", function name(req, res) {
+  res.send(`soy Santu y soy tremendo puto :v`);
 });
+
+app.listen(port);
