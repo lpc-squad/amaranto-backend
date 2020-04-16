cc91a7c8326d17486adf700c6f4b5eceb2f1eab5
FMGordillo <facundomgordillo@gmail.com>
2020-04-16 19:07:13 -0300

    FIX de... no c :v

--- resolvers.js
+++ resolvers.js
@@ -1,44 +1,44 @@
-const User = require('./models/user');
-const Institute = require('./models/institute')
-const Kind = require('graphql/language')
-const { GraphQLScalarType }= require('graphql')
-const dayjs = require('dayjs')
+const User = require("./models/user");
+const Institutes = require("./models/institute");
+const Kind = require("graphql/language");
+const { GraphQLScalarType } = require("graphql");
+const dayjs = require("dayjs");
 
-const userDB=
-[{id:"2",first_name: "pasdad",birth_date: new Date()},{id:"3",first_name: "qwedas",birth_date: new Date()},
-{id:"4",first_name: "adsas",birth_date: new Date()},{id:"5",first_name: "adewger",birth_date: new Date()}]
+const userDB = [
+  { id: "2", first_name: "pasdad", birth_date: new Date() },
+  { id: "3", first_name: "qwedas", birth_date: new Date() },
+  { id: "4", first_name: "adsas", birth_date: new Date() },
+  { id: "5", first_name: "adewger", birth_date: new Date() },
+];
 
 const resolvers = {
-    Query: {
-      users: () =>{
-        return userDB
-      },
-      user: (_, { first_name: name }) =>{
-        return userDB.find(obj=> obj.first_name=name)
-      },
-      institutes: () => {
-        Institute.find({},function getInstitutes(error,doc){//no funca lel
-          console.log(doc)
-        })
-
-      }
+  Query: {
+    users: () => {
+      return userDB;
     },
-    Date: new GraphQLScalarType({
-        name: 'Date',
-        description: 'Datetime',
-        serialize(value) {
-          return dayjs(value).format("MM-DD-YYYY");
-        },
-        parseValue(value) {
-          return dayjs(value);
-        },
-        parseLiteral(ast) {
-          if (ast.kind==Kind.INT) {
-              return dayjs(ast.value)
-          }
-          return null;
-        }
-      })
-  };
+    user: (_, { first_name: name }) => {
+      return userDB.find((obj) => (obj.first_name = name));
+    },
+    institutes: () => {
+      return Institutes.find({});
+    },
+  },
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "Datetime",
+    serialize(value) {
+      return dayjs(value).format("MM-DD-YYYY");
+    },
+    parseValue(value) {
+      return dayjs(value);
+    },
+    parseLiteral(ast) {
+      if (ast.kind == Kind.INT) {
+        return dayjs(ast.value);
+      }
+      return null;
+    },
+  }),
+};
 
 module.exports = resolvers;
