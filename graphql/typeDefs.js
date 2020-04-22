85ddbf3752c66c4694b3b6215ed8ab2fede199de
FMGordillo <facundomgordillo@gmail.com>
2020-04-21 23:37:56 -0300

    :lipstick: .js --> .graphql + FIX

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,6 +1,7 @@
-const path= require('path');
-const { fileLoader, mergeTypes } = require('merge-graphql-schemas');
- 
-const typesArray = fileLoader(path.join(__dirname, './types'));
- 
-module.exports= mergeTypes(typesArray, { all: true });
+const path = require("path");
+const { fileLoader, mergeTypes } = require("merge-graphql-schemas");
+
+const typesArray = fileLoader(path.join(__dirname, "./types"));
+console.log(typesArray);
+
+module.exports = mergeTypes(typesArray, { all: true });
