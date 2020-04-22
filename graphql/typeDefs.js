5095850295a580fa92e7571a6e7d244a0f007134
FMGordillo <facundomgordillo@gmail.com>
2020-04-21 23:47:14 -0300

    :wrench: Ultimo fix + ESLint

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -2,6 +2,5 @@ const path = require("path");
 const { fileLoader, mergeTypes } = require("merge-graphql-schemas");
 
 const typesArray = fileLoader(path.join(__dirname, "./types"));
-console.log(typesArray);
 
 module.exports = mergeTypes(typesArray, { all: true });
